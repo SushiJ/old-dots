66a43a6ef193f938b46254863338117a90ef09b9
Sushant Sharma <sushant.wurk@gmail.com>
2023-10-31 21:55:25 +0530

    awesomeWM fish

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -169,30 +169,38 @@ end)
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+
+-- awesome
   awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
+  awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
+  awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+  awful.key({ modkey }, "x", function()
+    awful.prompt.run({
+      prompt = "Run Lua code: ",
+      textbox = awful.screen.focused().mypromptbox.widget,
+      exe_callback = awful.util.eval,
+      history_path = awful.util.get_cache_dir() .. "/history_eval",
+    })
+  end, { description = "lua execute prompt", group = "awesome" }),
+
+  -- tag
   awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
   awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
   awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
+
+  -- client
   awful.key({ modkey }, "j", function()
     awful.client.focus.byidx(1)
   end, { description = "focus next by index", group = "client" }),
   awful.key({ modkey }, "k", function()
     awful.client.focus.byidx(-1)
   end, { description = "focus previous by index", group = "client" }),
-
-  -- Layout manipulation
   awful.key({ modkey, "Shift" }, "j", function()
     awful.client.swap.byidx(1)
   end, { description = "swap with next client by index", group = "client" }),
   awful.key({ modkey, "Shift" }, "k", function()
     awful.client.swap.byidx(-1)
   end, { description = "swap with previous client by index", group = "client" }),
-  awful.key({ modkey, "Control" }, "j", function()
-    awful.screen.focus_relative(1)
-  end, { description = "focus the next screen", group = "screen" }),
-  awful.key({ modkey, "Control" }, "k", function()
-    awful.screen.focus_relative(-1)
-  end, { description = "focus the previous screen", group = "screen" }),
   awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
   awful.key({ modkey }, "Tab", function()
     awful.client.focus.history.previous()
@@ -200,13 +208,26 @@ globalkeys = gears.table.join(
       client.focus:raise()
     end
   end, { description = "go back", group = "client" }),
+  awful.key({ modkey, "Control" }, "n", function()
+    local c = awful.client.restore()
+    -- Focus restored client
+    if c then
+      c:emit_signal("request::activate", "key.unminimize", { raise = true })
+    end
+  end, { description = "restore minimized", group = "client" }),
 
-  -- Standard program
+  -- launcher
   awful.key({ modkey }, "Return", function()
     awful.spawn(terminal)
   end, { description = "open a terminal", group = "launcher" }),
-  awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
-  awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+  awful.key({ modkey, "Shift" }, "a", function()
+    awful.spawn("pavucontrol")
+  end, { description = "Open pavucontrol", group = "launcher" }),
+  awful.key({ modkey }, "r", function()
+    awful.screen.focused().mypromptbox:run()
+  end, { description = "run prompt", group = "launcher" }),
+
+  -- layout
   awful.key({ modkey }, "l", function()
     awful.tag.incmwfact(0.05)
   end, { description = "increase master width factor", group = "layout" }),
@@ -232,29 +253,18 @@ globalkeys = gears.table.join(
     awful.layout.inc(-1)
   end, { description = "select previous", group = "layout" }),
 
-  awful.key({ modkey, "Control" }, "n", function()
-    local c = awful.client.restore()
-    -- Focus restored client
-    if c then
-      c:emit_signal("request::activate", "key.unminimize", { raise = true })
-    end
-  end, { description = "restore minimized", group = "client" }),
-
-  -- Prompt
-  awful.key({ modkey }, "r", function()
-    awful.screen.focused().mypromptbox:run()
-  end, { description = "run prompt", group = "launcher" }),
-
-  awful.key({ modkey }, "x", function()
-    awful.prompt.run({
-      prompt = "Run Lua code: ",
-      textbox = awful.screen.focused().mypromptbox.widget,
-      exe_callback = awful.util.eval,
-      history_path = awful.util.get_cache_dir() .. "/history_eval",
-    })
-  end, { description = "lua execute prompt", group = "awesome" })
+  -- screen
+  awful.key({ modkey, "Control" }, "j", function()
+    awful.screen.focus_relative(1)
+  end, { description = "focus the next screen", group = "screen" }),
+  awful.key({ modkey, "Control" }, "k", function()
+    awful.screen.focus_relative(-1)
+  end, { description = "focus the previous screen", group = "screen" })
 )
 
+-- Set keys
+root.keys(globalkeys)
+-- }}}
 clientkeys = gears.table.join(
   awful.key({ modkey }, "f", function(c)
     c.fullscreen = not c.fullscreen
@@ -297,9 +307,6 @@ clientkeys = gears.table.join(
   end, { description = "(un)maximize horizontally", group = "client" })
 )
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
   globalkeys = gears.table.join(
     globalkeys,
@@ -353,10 +360,6 @@ clientbuttons = gears.table.join(
     awful.mouse.client.resize(c)
   end)
 )
-
--- Set keys
-root.keys(globalkeys)
--- }}}
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
