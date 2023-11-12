f92ba72c9b3caa29ca389215c9e3cf6ba5a7f2df
Sushant Sharma <sushant.wurk@gmail.com>
2023-11-12 13:42:29 +0530

    nvim formatting stuff

--- nvim/.config/nvim/lua/sushi/plugins/formatter.lua
+++ nvim/.config/nvim/lua/sushi/plugins/formatter.lua
@@ -2,15 +2,20 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
     opts = function()
       local nls = require("null-ls")
       return {
         sources = {
+          nls.builtins.formatting.prettierd,
           nls.builtins.formatting.beautysh,
           nls.builtins.formatting.stylua,
-          nls.builtins.formatting.prettier,
-          nls.builtins.formatting.prettierd,
+
+          nls.builtins.diagnostics.eslint_d.with({
+            diagnostics_format = '[eslint] #{m}\n(#{c})'
+          }),
+
+          nls.builtins.code_actions.eslint_d,
+          nls.builtins.completion.spell
         },
       }
     end,
