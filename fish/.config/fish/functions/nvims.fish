32673cfad279a8904690c151fe3bee51d722a6d8
Sushant Sharma <sushant.wurk@gmail.com>
2024-03-01 01:09:31 +0530

    Chore: remove unnecessary nvim flavors

--- fish/.config/fish/functions/nvims.fish
+++ fish/.config/fish/functions/nvims.fish
@@ -1,12 +1,7 @@
-alias nvim-lazyVim="NVIM_APPNAME=LazyVim nvim"
-alias nvim-lazy="NVIM_APPNAME=LazyNvim nvim"
 alias nvim-kick="NVIM_APPNAME=Kickstart nvim"
-alias nvim-chad="NVIM_APPNAME=NvChad nvim"
-alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
-alias nvim-lunar="NVIM_APPNAME=LunarVim nvim"
 
 function nvims 
-    set items "default" "Kickstart" "LazyVim" "LazyNvim" "NvChad" "AstroNvim" "LunarVim"
+    set items "default" "Kickstart"
     set config (printf  "%s\n" $items | fzf --prompt=" Neovim Config " --height=56% --layout=reverse --border --exit-0)
     if [ -z $config ]
         echo "Nothing selected"
